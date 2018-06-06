3f302a595164378b8c7ee7dedcd309cf5700ca6d
CHAD STACHOWICZ <chadstachowicz@CHADs-iMac.local>
2018-06-05 22:38:06 -0400

    sdfdfsf

--- src/App.js
+++ src/App.js
@@ -139,7 +139,7 @@ function SforceScreenPop(){
     }
   };
 //Invokes API method
-  sforce.opencti.searchAndScreenPop({ searchParams : 'Chad',queryParams : '', callType : sforce.opencti.CALL_TYPE.INBOUND, deferred: false, callback : callback });
+  window.sforce.opencti.searchAndScreenPop({ searchParams : 'Chad',queryParams : '', callType : window.sforce.opencti.CALL_TYPE.INBOUND, deferred: false, callback : callback });
 }
 function openFrameInitSuccess(snConfig) {
   window.openFrameConfig = snConfig;
