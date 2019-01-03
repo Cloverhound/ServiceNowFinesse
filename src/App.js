2e0b0a7d52dc21411ef9119c0ba2438f066c7916
CHAD STACHOWICZ <chadstachowicz@CHADs-iMac.local>
2019-01-03 01:18:20 -0500

    woring on zendesk

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ if (clientType === "sforce") {
   script.onload = function () {
     loadPlugin();
     zaf_client = window.ZAFClient.init();
-    zaf_client.invoke('resize', { width: '300px', height: '500px' });
+    zaf_client.invoke('resize', { width: '300px', height: '400px' });
   };
   script.src = "https://static.zdassets.com/zendesk_app_framework_sdk/2.0/zaf_sdk.min.js";
 
